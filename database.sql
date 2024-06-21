cf668df848fd368ad293f92e6858a683ce86e9d5
Eduin-M <eduinmeneses705@gmail.com>
2024-06-20 21:43:18 -0400

    .

--- database.sql
+++ database.sql
@@ -44,8 +44,36 @@ CREATE TABLE `paciente` (
 
 LOCK TABLES `paciente` WRITE;
 /*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
-INSERT INTO `paciente` VALUES (11222333,'Manuel','Rodriguez','2004-08-20',20,412888888,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1),(14555444,'Anthoan','Gonzalez','1958-04-02',18,412888888,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1),(22333111,'jose','sanchez','1888-12-01',25,412888888,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1),(24550967,'Jorge Luis','Patiño Torres','1999-02-20',28,412075429,'Gerente de Negros','Pedro Leon Torres & Calle 58',1),(30111458,'Juan','Chacon','2004-05-20',20,412075429,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1);
+INSERT INTO `paciente` VALUES (11222333,'Manuel','Rodriguez','2004-08-20',20,412888888,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1),(14555444,'Anthoan','Gonzalez','1958-04-02',18,412888888,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1),(24550967,'Jorge Luis','Patiño Torres','1999-02-20',28,412075429,'Gerente de Negros','Pedro Leon Torres & Calle 58',1),(30111458,'Juan','Chacon','2004-05-20',20,412075429,'jefe','cerritos blanco vereda 13 entre calles 1 y 2',1);
 /*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `cedula` int NOT NULL,
+  `nombre` varchar(45) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `apellido` varchar(45) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `correo` varchar(45) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  `rol` varchar(45) CHARACTER SET utf8mb3 DEFAULT NULL,
+  PRIMARY KEY (`cedula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='tabla del personal';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES (30156845,'andres','mendoza','andres@gmail.com',987654321,'2');
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-18 21:18:41
+-- Dump completed on 2024-06-20 21:38:18
